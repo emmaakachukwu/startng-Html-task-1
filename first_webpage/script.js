348de7d0f1ec412b1a9cdadb2bfb5cebda03aaeb
emmanuel akachukwu <emmanuelakachukwu1@gmail.com>
2020-03-23 13:51:12 +0100

    first commit

---
+++ first_webpage/script.js
@@ -0,0 +1,18 @@
+var name = 'emmaakachukwu';
+var courses = [
+    'html',
+    'css',
+    'javascript',
+    'php',
+    'python',
+    'c-sharp',
+    'nodejs'
+]
+
+for ( i = 1; i <= 200; i++ ) {
+    if ( i % 2 ) {
+        console.log(i);
+    } else {
+        continue;
+    }
+}
